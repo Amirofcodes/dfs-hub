c0c26b863d9ea1145f5bd279febc380bd93df0f4
Amirofcodes <j.bouddehbine@it-students.fr>
2024-10-03 09:17:00 +0200

    Update navigation, handle file uploads for learning resources

--- src/Controller/LearningResourceController.php
+++ src/Controller/LearningResourceController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 #[Route('/learn')]
 class LearningResourceController extends AbstractController
@@ -25,7 +27,7 @@ class LearningResourceController extends AbstractController
 
     #[Route('/new', name: 'app_learning_resource_new', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $learningResource = new LearningResource();
         $form = $this->createForm(LearningResourceType::class, $learningResource);
@@ -33,9 +35,30 @@ class LearningResourceController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $learningResource->setAuthor($this->getUser());
+
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('learning_resources_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'There was an error uploading the image. Please try again.');
+                    return $this->redirectToRoute('app_learning_resource_new');
+                }
+
+                $learningResource->setImageFilename($newFilename);
+            }
+
             $entityManager->persist($learningResource);
             $entityManager->flush();
 
+            $this->addFlash('success', 'The learning resource has been created successfully.');
             return $this->redirectToRoute('app_learning_resource_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -55,15 +78,43 @@ class LearningResourceController extends AbstractController
 
     #[Route('/{id}/edit', name: 'app_learning_resource_edit', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_ADMIN')]
-    public function edit(Request $request, LearningResource $learningResource, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, LearningResource $learningResource, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(LearningResourceType::class, $learningResource);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('learning_resources_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'There was an error uploading the image. Please try again.');
+                    return $this->redirectToRoute('app_learning_resource_edit', ['id' => $learningResource->getId()]);
+                }
+
+                // Delete the old image file if it exists
+                if ($learningResource->getImageFilename()) {
+                    $oldImagePath = $this->getParameter('learning_resources_directory').'/'.$learningResource->getImageFilename();
+                    if (file_exists($oldImagePath)) {
+                        unlink($oldImagePath);
+                    }
+                }
+
+                $learningResource->setImageFilename($newFilename);
+            }
+
             $learningResource->setUpdatedAt(new \DateTime());
             $entityManager->flush();
 
+            $this->addFlash('success', 'The learning resource has been updated successfully.');
             return $this->redirectToRoute('app_learning_resource_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -78,8 +129,18 @@ class LearningResourceController extends AbstractController
     public function delete(Request $request, LearningResource $learningResource, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$learningResource->getId(), $request->request->get('_token'))) {
+            // Delete the image file if it exists
+            if ($learningResource->getImageFilename()) {
+                $imagePath = $this->getParameter('learning_resources_directory').'/'.$learningResource->getImageFilename();
+                if (file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+            }
+
             $entityManager->remove($learningResource);
             $entityManager->flush();
+
+            $this->addFlash('success', 'The learning resource has been deleted successfully.');
         }
 
         return $this->redirectToRoute('app_learning_resource_index', [], Response::HTTP_SEE_OTHER);
